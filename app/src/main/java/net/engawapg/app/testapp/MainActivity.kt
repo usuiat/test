99ce1994adc3019ed0ab678016a7d15ef4a4b898
usuiat <stripewani@gmail.com>
2023-02-20 22:51:23 +0900

    テストプロジェクト。dokkaHtml含む。

---
+++ app/src/main/java/net/engawapg/app/testapp/MainActivity.kt
@@ -0,0 +1,53 @@
+package net.engawapg.app.testapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import net.engawapg.app.testapp.ui.theme.TestAppTheme
+
+/**
+ * MainActivity
+ *
+ * This is a main activity.
+ */
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TestAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+/**
+ * Greeting function
+ *
+ * @param name The name
+ */
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TestAppTheme {
+        Greeting("Android")
+    }
+}
